15991a032487fc7fb014813fb9de37b4836da13b
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2023-02-11 00:29:33 +0200

    added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,41 +1,5 @@
 #!/usr/bin/node
-
-
-
 const fs = require('fs');
-
-
-
-function readFile(filePath) {
-
-  fs.readFile(filePath, 'utf-8', (error, data) => {
-
-    if (error) {
-
-      console.error(`An error occurred while reading the file: ${error}`);
-
-    } else {
-
-      console.log(data);
-
-    }
-
-  });
-
-}
-
-
-
-if (process.argv.length >= 3) {
-
-  const filePath = process.argv[2];
-
-  readFile(filePath);
-
-} else {
-
-  console.error('Please provide a file path as an argument.');
-
-}
-
-
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
